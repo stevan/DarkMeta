db7ad3148f26fb78e7f02575ee72adc6d9329561
Stevan Little <stevan.little@booking.com>
2016-03-18 12:59:52 +0100

    remove the keyword plugin, didn’t really like it anyway

--- root/static/js/prism.js
+++ root/static/js/prism.js
@@ -684,21 +684,3 @@ Prism.hooks.add('complete', function (env) {
 });
 
 }());
-(function(){
-
-if (
-	typeof self !== 'undefined' && !self.Prism ||
-	typeof global !== 'undefined' && !global.Prism
-) {
-	return;
-}
-
-Prism.hooks.add('wrap', function(env) {
-	if (env.type !== "keyword") {
-		return;
-	}
-	env.classes.push('keyword-' + env.content);
-});
-
-})();
-
